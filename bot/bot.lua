72360070278758387fae8ff6b1d02d620f4ceb43
kiarash14 <omidbakhsh22@gmail.com>
2016-03-28 07:15:38 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -246,7 +246,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {100096055},
     admin_users = {},
     disabled_channels = {}
   }
